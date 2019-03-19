3a90159c025bcb9bf7499eb1e8566bdaf88cb27e
Ali Madooei <alimadooei@Alis-MacBook-Pro.local>
2019-03-18 21:11:48 -0700

    added lecture java files

---
+++ java-project/src/main/java/lecture/CanReachFood.java
@@ -0,0 +1,55 @@
+package lecture;
+
+public class CanReachFood {
+    private static final int FOOD = 2;   // Destination
+    private static final int BLOCK = -1; // Obstacle
+
+    // REQUIRES: maze is a 2D array where road blocks are indicated by -1
+    //           and the bottom right corner is marked as 2
+    // EFFECTS: returns true if we can start at top left corner of maze and
+    //  get to the bottom right corner while moving only one step down or right.
+    public static boolean can_reach_food(int[][] maze){
+        return can_reach_food(maze, 0, 0);
+    }
+
+    // REQUIRES: maze is a 2D array where road blocks are indicated by -1
+    //           and the bottom right corner is marked as 2
+    // EFFECTS: returns true if we can start at top left corner of maze and
+    //          get to the bottom right corner while moving only one step down or right.
+    private static boolean can_reach_food(int[][] maze, int row, int col) {
+        return false; // stub
+    }
+
+    // REQUIRES: row and col are valid indices of maze
+    //           maze is a 2D array where road blocks are indicated by -1
+    //           and the bottom right corner is marked as 2
+    // EFFECTS: returns true if maze[row][col] is the destination (bottom right corner is marked as 2)
+    private static boolean is_food(int[][] maze, int row, int col) {
+        return maze[row][col] == FOOD;
+    }
+
+    // REQUIRES: maze is a 2D array where road blocks are indicated by -1
+    //           and the bottom right corner is marked as 2
+    // EFFECTS: returns true if col+1 is within maze bound and is not blocked
+    private static boolean can_move_right(int[][] maze, int row, int col) {
+        return col >= 0 && col+1 < maze[0].length && maze[row][col+1] != BLOCK;
+    }
+
+    // REQUIRES: maze is a 2D array where road blocks are indicated by -1
+    //           and the bottom right corner is marked as 2
+    // EFFECTS: returns true if row+1 is within maze bound and is not blocked
+    private static boolean can_move_down(int[][] maze, int row, int col) {
+        return row >= 0 && row+1 < maze.length && maze[row+1][col] != BLOCK;
+    }
+
+    public static void main(String[] args) {
+        int[][] maze = new int[][]{{0,1,1},{1,1,1},{1,1,2}};
+        System.out.println(can_reach_food(maze));
+
+        maze = new int[][]{{0,1,-1},{1,-1,1},{1,1,2}};
+        System.out.println(can_reach_food(maze));
+
+        maze = new int[][]{{0,1,1},{1,1,-1},{1,-1,2}};
+        System.out.println(can_reach_food(maze));
+    }
+}
--- java-project/src/main/java/lecture/CountPath.java
+++ java-project/src/main/java/lecture/CountPath.java
@@ -1,4 +1,4 @@
-package solution;
+package lecture;
 
 // Goal: count how many different paths one can take in order to get from
 // the top left corner to the bottom right corner of a grid.
@@ -9,13 +9,7 @@ public class CountPath {
     // REQUIRES: width >= 1 && height >= 1
     // EFFECTS: returns the count of paths
     public static int count_paths(int width, int height) {
-        if (width == 1 || height == 1){
-            return 1;
-        } else {
-            int go_right = count_paths(width - 1, height);
-            int go_up = count_paths(width, height - 1);
-            return go_right + go_up;
-        }
+        return 0; // stub
     }
 
     public static void main(String[] args) {
